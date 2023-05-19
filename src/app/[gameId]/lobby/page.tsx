02cc8c2c6a6003c28671d1012a37d5a103dd429f
Ibrahim Taib <ibrahim.taib@grenoble-inp.org>
2023-05-19 15:29:01 +0200

    first draft for lobby skeleton
    we have 5 cards for 5 players, and a card container

--- src/app/[gameId]/lobby/page.tsx
+++ src/app/[gameId]/lobby/page.tsx
@@ -3,11 +3,13 @@ import style from 'styles/lobby.module.css';
 function Lobby() {
   return (
     <div className={style.lobby}>
-      <Card className={style.card1} />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+      <div className={style.cardContainer}>
+        <Card className={[style.card, style.card1].join(' ')} />
+        <Card className={[style.card, style.card2].join(' ')} />
+        <Card className={[style.card, style.card3].join(' ')} />
+        <Card className={[style.card, style.card4].join(' ')} />
+        <Card className={[style.card, style.card5].join(' ')} />
+      </div>
     </div>
   );
 }
