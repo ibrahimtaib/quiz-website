7117ed2b20a9b77f58689286ace12e3bd4669aad
Ibrahim Taib <ibrahim.taib@grenoble-inp.org>
2023-05-20 15:30:23 +0200

    adding clicked styling

--- src/packages/components/ColorChoice.tsx
+++ src/packages/components/ColorChoice.tsx
@@ -1,7 +1,7 @@
 import { MouseEvent, useMemo, useState } from 'react';
+import { AiOutlineCheck } from 'react-icons/ai';
 import style from 'styles/colorChoice.module.css';
 import { ColorChoiceProps } from 'types/colors';
-
 function ColorChoice({
   className,
   colorScheme,
@@ -18,11 +18,9 @@ function ColorChoice({
   }
   const colorStyle = useMemo(() => {
     return {
-      backgroundColor: colorScheme.primaryColor,
-      filter: buttonClicked === true ? 'brightness(50%)' : '',
-      cursor: buttonClicked === false ? 'pointer' : 'initial'
+      backgroundColor: colorScheme.primaryColor
     };
-  }, [buttonClicked, colorScheme.primaryColor]);
+  }, [colorScheme.primaryColor]);
   const onClickFunction = (e: MouseEvent) => {
     e.preventDefault();
     setButtonClicked(true);
@@ -31,12 +29,19 @@ function ColorChoice({
     }
   };
   return (
-    <div className={[style.colorChoiceContainer, className].join(' ')}>
+    <div className={[style.colorChoiceContainer, className].join(' ').trim()}>
       <button
-        className={style.colorChoice}
+        className={[
+          style.colorChoice,
+          buttonClicked === true ? style.clicked : ''
+        ]
+          .join(' ')
+          .trim()}
         style={colorStyle}
         onClick={onClickFunction}
-      />
+      >
+        {buttonClicked && <AiOutlineCheck color="white" size={23} />}
+      </button>
     </div>
   );
 }
