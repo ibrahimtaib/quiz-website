662e147ba6725b5556221e6d361d014a3d438c84
Ibrahim Taib <ibrahim.taib@grenoble-inp.org>
2023-05-21 01:03:17 +0200

    factorings cards code into component

--- src/app/[gameId]/lobby/page.tsx
+++ src/app/[gameId]/lobby/page.tsx
@@ -1,23 +1,15 @@
 'use client';
-import Card from 'components/Card';
+import LobbyCards from 'components/LobbyCards';
 import LobbyColorPicker from 'components/LobbyColorPicker';
 import { useState } from 'react';
 import style from 'styles/lobby.module.css';
 import { COLORS, ColorScheme } from 'types/colors';
 function Lobby() {
   const [userColor, setUserColor] = useState<ColorScheme>(COLORS.YELLOW);
+
   return (
     <div className={style.lobby}>
-      <div className={style.cardContainer}>
-        <Card className={[style.card, style.card1].join(' ')} />
-        <Card className={[style.card, style.card2].join(' ')} />
-        <Card className={[style.card, style.card3].join(' ')} />
-        <Card className={[style.card, style.card4].join(' ')} />
-        <Card
-          className={[style.card, style.card5].join(' ')}
-          colorScheme={userColor}
-        />
-      </div>
+      <LobbyCards userColor={userColor} />
       <LobbyColorPicker
         userColor={userColor}
         setUserColor={setUserColor}
---
+++ src/packages/components/LobbyCards.tsx
@@ -0,0 +1,42 @@
+import style from 'styles/lobbycards.module.css';
+import { COLORS, ColorScheme } from 'types/colors';
+import Card from './Card';
+import ProfilePicture from './ProfilePicture';
+
+function LobbyCards({ userColor }: { userColor: ColorScheme }) {
+  const styles = [
+    style.card1,
+    style.card2,
+    style.card3,
+    style.card4,
+    style.card5
+  ];
+  const colors = [
+    COLORS.PURPLE,
+    COLORS.BLUE,
+    COLORS.RED,
+    COLORS.GREEN,
+    userColor
+  ];
+  return (
+    <div className={style.cardContainer}>
+      {styles.map((s, index) => {
+        return (
+          <Card
+            key={s}
+            className={[style.card, s].join(' ').trim()}
+            colorScheme={colors[index]}
+          >
+            <ProfilePicture
+              colorScheme={colors[index]}
+              className={style.profilePicture}
+            />
+          </Card>
+        );
+      })}
+      ;
+    </div>
+  );
+}
+
+export default LobbyCards;
