ef22461215921b2f97d78813a9760a38e36f033c
Ibrahim Taib <ibrahim.taib@grenoble-inp.org>
2023-05-24 22:22:02 +0200

    cleaning code

--- src/app/[gameId]/page.tsx
+++ src/app/[gameId]/page.tsx
@@ -1,30 +1,10 @@
 'use client';
-import Card from 'components/Card';
-import style from 'styles/ingame.module.css';
-import { ColorScheme, FALSE, TRUE } from 'types/colors';
+
+import Question from 'components/Question';
 
 /* questions page */
 function Page() {
-  const white: ColorScheme = {
-    primaryColor: '#ffffff',
-    name: '',
-    secondaryColor: '',
-    tertiaryColor: '',
-    quaternaryColor: ''
-  };
-  return (
-    <div className={style.container}>
-      <Card
-        className={style.question}
-        colorScheme={white}
-        horizontal={true}
-      ></Card>
-      <div className={style.answers}>
-        <Card colorScheme={TRUE} className={style.answer}></Card>
-        <Card colorScheme={FALSE} className={style.answer}></Card>
-      </div>
-    </div>
-  );
+  return <Question></Question>;
 }
 
 export default Page;
