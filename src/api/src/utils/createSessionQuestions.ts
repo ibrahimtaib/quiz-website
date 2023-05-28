313aa2261094727d417459ee5292e91e3e9b9a0e
Ibrahim Taib <ibrahim.taib@grenoble-inp.org>
2023-05-28 19:02:33 +0200

    now returning promise

--- src/api/src/utils/createSessionQuestions.ts
+++ src/api/src/utils/createSessionQuestions.ts
@@ -1,6 +1,7 @@
+import fetch from 'node-fetch';
 import { TriviaQuestion } from '../../../packages/types/api';
 export const createSessionQuestions = async (): Promise<TriviaQuestion[]> => {
-  await fetch('https://opentdb.com/api.php?amount=10&type=multiple')
+  return await fetch('https://opentdb.com/api.php?amount=10&type=multiple')
     .then(res => {
       if (!res.ok) {
         throw new Error('Could not fetch questions');
@@ -8,7 +9,6 @@ export const createSessionQuestions = async (): Promise<TriviaQuestion[]> => {
       return res.json();
     })
     .then(data => {
-      return data.results;
+      return (data as any).results;
     });
-  return [];
 };
