6df9c0efd6b27a106525e7a03ae2fb0b54392ff4
Ibrahim Taib <ibrahim.taib@grenoble-inp.org>
2023-05-29 01:42:42 +0200

    adding controller to answer questions
    adding a controller that allows the user to answer the current question
    for a given session

--- src/api/src/controller/session.ts
+++ src/api/src/controller/session.ts
@@ -1,4 +1,9 @@
-import { NewSession, TriviaQuestion } from '../../../packages/types/api';
+import {
+  NewSession,
+  PlayerChoice,
+  Question,
+  TriviaQuestion
+} from '../../../packages/types/api';
 import prisma from '../prisma';
 import { createSessionQuestions } from '../utils/createSessionQuestions';
 
@@ -11,7 +16,6 @@ const sessionController = {
       const questions: TriviaQuestion[] = await createSessionQuestions(
         newSession
       );
-      console.log(questions);
       const session = await prisma.session.create({
         data: {
           questions: {
@@ -32,12 +36,138 @@ const sessionController = {
 
       res
         .status(200)
-        .json({ message: 'New session createed successfully!', session });
+        .json({ message: 'New session created successfully!', session });
     } catch (error) {
       console.error(error);
       res.status(500).json({
         error:
           'Could not create a new session. Please contact the administrator if the problem persists'
+      });
+    }
+  },
+  answerCurrentQuestion: async (req: any, res: any) => {
+    const { gameId, sessionId } = req.params;
+    const { playerId, answer } = req.body;
+
+    try {
+      await prisma.$transaction(async prisma => {
+        const session = await prisma.session.findUnique({
+          where: {
+            id: +sessionId
+          },
+          include: {
+            questions: true
+          }
+        });
+
+        if (!session) {
+          res.status(404).json({
+            error: 'Session not found'
+          });
+          return;
+        }
+
+        /* Check validity of currQuestion */
+        if (session.currQuestion >= session.questions.length) {
+          res.status(400).json({
+            error: 'No more questions to answer'
+          });
+          return;
+        }
+        const currentQuestion: Question =
+          session.questions[session.currQuestion];
+
+        /* Check if the answer is valid */
+        const isCorrect = currentQuestion.correct_answer === answer;
+        if (!isCorrect && !currentQuestion.incorrect_answers.includes(answer)) {
+          res.status(400).json({
+            error: 'Answer is not valid'
+          });
+          return;
+        }
+        //Check if playerId can be turned into a number
+        if (isNaN(parseInt(playerId))) {
+          res.status(400).json({
+            error: 'PlayerId is not valid'
+          });
+          return;
+        }
+        const player = await prisma.player.findUnique({
+          where: {
+            id: +playerId
+          }
+        });
+
+        if (!player) {
+          res.status(404).json({
+            error: 'Player not found'
+          });
+          return;
+        }
+
+        /* Check if player has already answered the question */
+        let playerChoice: PlayerChoice | null =
+          await prisma.playerChoice.findUnique({
+            where: {
+              playerId_questionId: {
+                playerId: +playerId,
+                questionId: currentQuestion.id
+              }
+            }
+          });
+        let offset = 0;
+        if (playerChoice === null) {
+          playerChoice = await prisma.playerChoice.create({
+            data: {
+              player: {
+                connect: {
+                  id: +playerId
+                }
+              },
+              question: {
+                connect: {
+                  id: currentQuestion.id
+                }
+              },
+              answer
+            }
+          });
+          offset = isCorrect ? 1 : 0;
+        } else {
+          const wasCorrect =
+            playerChoice.answer === currentQuestion.correct_answer;
+          playerChoice = await prisma.playerChoice.update({
+            where: {
+              playerId_questionId: {
+                playerId: +playerId,
+                questionId: currentQuestion.id
+              }
+            },
+            data: {
+              answer
+            }
+          });
+          offset = wasCorrect && !isCorrect ? -1 : 0;
+        }
+        await prisma.player.update({
+          where: {
+            id: +playerId
+          },
+          data: {
+            score: player.score + offset
+          }
+        });
+
+        res.status(200).json({
+          message: 'Question answered successfully',
+          playerChoice
+        });
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({
+        error:
+          'Could not answer the question. Please contact the administrator if the problem persists'
       });
     }
   }
--- src/api/src/routes/game.ts
+++ src/api/src/routes/game.ts
@@ -2,10 +2,17 @@ import express from 'express';
 import gameController from '../controller/game';
 import sessionController from '../controller/session';
 import { verifyGameId } from '../middleware/games';
+import { verifySessionId } from '../middleware/session';
 const router = express.Router();
 router.use('/:gameId', verifyGameId);
 router.post('/', gameController.createGame);
 router.get('/:gameId', gameController.getGame);
 
 router.post('/:gameId/sessions', sessionController.createSession);
+
+router.use('/:gameId/sessions/:sessionId', verifySessionId);
+router.post(
+  '/:gameId/sessions/:sessionId/',
+  sessionController.answerCurrentQuestion
+);
 export default router;
