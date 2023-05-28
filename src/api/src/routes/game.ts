e2047094e3c3ad2802439f8d28be16a8a2d452ac
Ibrahim Taib <ibrahim.taib@grenoble-inp.org>
2023-05-28 19:03:56 +0200

    adding post route for sessions

--- src/api/src/routes/game.ts
+++ src/api/src/routes/game.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import gameController from '../controller/game';
+import sessionController from '../controller/session';
 import { verifyGameId } from '../middleware/games';
 const router = express.Router();
 router.use('/:gameId', verifyGameId);
 router.post('/', gameController.createGame);
 router.get('/:gameId', gameController.getGame);
 
+router.post('/:gameId/sessions', sessionController.createSession);
 export default router;
