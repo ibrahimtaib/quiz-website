1e1581bb104ce43e61f64f6bc444fbec09258368
Ibrahim Taib <ibrahim.taib@grenoble-inp.org>
2023-05-28 19:02:49 +0200

    now checking if gameid exists

--- src/api/src/middleware/games.ts
+++ src/api/src/middleware/games.ts
@@ -1,10 +1,21 @@
 import { NextFunction } from 'express';
+import prisma from '../prisma';
 
 export const verifyGameId = async (req: any, res: any, next: NextFunction) => {
   const { gameId } = req.params;
   const value = parseInt(gameId);
   if (isNaN(value) || value < 0) {
     return res.status(400).json({ error: 'Invalid game ID' });
+  }
+
+  const game = await prisma.game.findUnique({
+    where: {
+      id: value
+    }
+  });
+
+  if (!game) {
+    return res.status(404).json({ error: 'Game not found' });
   }
   next();
 };
