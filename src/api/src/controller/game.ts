5e1d3d433c3a0d5b4079afdc2fecacc142a25471
Ibrahim Taib <ibrahim.taib@grenoble-inp.org>
2023-05-28 17:40:10 +0200

    game creation controller

--- src/api/src/controller/game.ts
+++ src/api/src/controller/game.ts
@@ -1,28 +1,45 @@
-import { Game } from '../../../packages/types/api';
+import { COLORS, ColorScheme } from '../../../packages/types/colors';
 import prisma from '../prisma';
 const gameController: any = {
   createGame: async (req: any, res: any) => {
-    const game: Game = req.body;
-
-    const myGame = await prisma.game.create({
-      data: {
-        players: {},
-        sessions: {
-          create: {
-            questions: {}
+    const { username, color }: { username: string; color: string } = req.body;
+    const colorNames: String[] = Object.values<ColorScheme>(COLORS).map(
+      (c: ColorScheme) => c.name.toLowerCase()
+    );
+    if (!colorNames.includes(color.toLowerCase())) {
+      res.status(400).json({
+        error: `Invalid color. Please choose from: ${colorNames.join(', ')}`
+      });
+      return;
+    }
+    try {
+      const myGame = await prisma.game.create({
+        data: {
+          players: {
+            create: {
+              username: username,
+              color: color
+            }
           },
-          orderBy: {
-            id: 'asc'
+          sessions: {
+            create: {
+              questions: {}
+            }
           }
+        },
+        include: {
+          players: true,
+          sessions: true
         }
-      },
-      include: {
-        players: true,
-        sessions: true
-      }
-    });
+      });
 
-    res.json(myGame);
+      res.status(200).json(myGame);
+    } catch (error) {
+      res.status(500).json({
+        error:
+          'Could not create a game. Please contact an administrator if the error persists'
+      });
+    }
   }
 };
 
