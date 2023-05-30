c47ed600381aec1cfb63601a494a5c5df7419c58
Ibrahim Taib <ibrahim.taib@grenoble-inp.org>
2023-05-30 10:52:45 +0200

    allowing cors

--- src/api/src/server.ts
+++ src/api/src/server.ts
@@ -1,11 +1,12 @@
+import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
 import router from './routes/router';
-
 dotenv.config();
 const app = express();
 
 // Define middleware and routes here
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use('/api', router);
