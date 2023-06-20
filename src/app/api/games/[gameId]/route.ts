2f4ea5951af2772564760d34cfd3fb2a95d2dc2b
Ibrahim Taib <ibrahim.taib@grenoble-inp.org>
2023-06-20 15:42:44 +0200

    api route to create new player in game

--- src/app/api/games/[gameId]/route.ts
+++ src/app/api/games/[gameId]/route.ts
@@ -1,4 +1,5 @@
 import { NextResponse } from 'next/server';
+import { GameStatus } from 'types/api';
 import prisma from 'utils/prisma';
 export async function GET(
   request: Request,
@@ -36,4 +37,66 @@ export async function GET(
       { status: 500 }
     );
   }
+}
+
+export async function POST(
+  request: Request,
+  {
+    params
+  }: {
+    params: { gameId: string };
+  }
+) {
+  const { username, color } = await request.json();
+  const gameId = params.gameId;
+  const game = await prisma.game.findUnique({
+    where: {
+      id: +gameId
+    },
+    include: {
+      players: true
+    }
+  });
+
+  if (!game) {
+    return NextResponse.json({ message: 'Game not found' }, { status: 404 });
+  }
+  if (game.players.length == 5) {
+    return NextResponse.json({ message: 'Game is full' }, { status: 400 });
+  }
+  if (game.status !== GameStatus.LOBBY) {
+    return NextResponse.json(
+      { message: 'Game has already started' },
+      { status: 400 }
+    );
+  }
+  return prisma.game
+    .update({
+      where: {
+        id: +gameId
+      },
+      data: {
+        players: {
+          create: {
+            username: username,
+            color: color
+          }
+        }
+      }
+    })
+    .then(() => {
+      return NextResponse.json(
+        { message: 'Game joined successfully!' },
+        { status: 200 }
+      );
+    })
+    .catch(() => {
+      return NextResponse.json(
+        {
+          error:
+            'Could not join game. Please contact an administrator if the error persists'
+        },
+        { status: 500 }
+      );
+    });
 }
